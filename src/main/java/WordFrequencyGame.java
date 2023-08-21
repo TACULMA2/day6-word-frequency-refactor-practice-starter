ffebb6e8339c8eda2e16e5e1819d37d677bb4273
TACULMA2 <marigold.taculog@oocl.com>
2023-08-21 18:28:33 +0800

    refactor: removed extra lines, used reformat line, rename the magic strings into constant variables.

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,29 +8,30 @@ import java.io.CharArrayWriter;
 import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr){
 
+    public static final String SPACE_DELIMETER = "\\s+";
+    public static final String NEWLINE_DELIMITER = "\n";
+    public static final String SPACE_CHAR = " ";
+    public static final String CALCULATE_ERROR = "Calculate Error";
 
-        if (inputStr.split("\\s+").length==1) {
+    public String getResult(String inputStr) {
+        if (inputStr.split(SPACE_DELIMETER).length == 1) {
             return inputStr + " 1";
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = inputStr.split(SPACE_DELIMETER);
 
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
-
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map =getListMap(inputList);
+                Map<String, List<Input>> map = getListMap(inputList);
 
                 List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()){
+                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
@@ -38,37 +39,30 @@ public class WordFrequencyGame {
 
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                StringJoiner joiner = new StringJoiner("\n");
+                StringJoiner joiner = new StringJoiner(NEWLINE_DELIMITER);
                 for (Input w : inputList) {
-                    String s = w.getValue() + " " +w.getWordCount();
+                    String s = w.getValue() + SPACE_CHAR + w.getWordCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
             } catch (Exception e) {
-
-
-                return "Calculate Error";
+                return CALCULATE_ERROR;
             }
         }
     }
 
-
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
+    private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
+        for (Input input : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())){
+            if (!map.containsKey(input.getValue())) {
                 ArrayList arr = new ArrayList<>();
                 arr.add(input);
                 map.put(input.getValue(), arr);
-            }
-
-            else {
+            } else {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
 

d7345f21e1c85e9d85ab262639b938a6aeba4a80
TACULMA2 <marigold.taculog@oocl.com>
2023-08-21 22:37:39 +0800

    refactor: refactor getListMap into createWordFrequencyMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,32 +3,23 @@ import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
 
-    public static final String SPACE_DELIMETER = "\\s+";
+    public static final String SPACE_DELIMITER = "\\s+";
     public static final String NEWLINE_DELIMITER = "\n";
     public static final String SPACE_CHAR = " ";
     public static final String CALCULATE_ERROR = "Calculate Error";
 
     public String getResult(String inputText) {
-        if (inputText.split(SPACE_DELIMETER).length == 1) {
+        if (inputText.split(SPACE_DELIMITER).length == 1) {
             return inputText + " 1";
         } else {
             try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = inputText.split(SPACE_DELIMETER);
+                String[] words = inputText.split(SPACE_DELIMITER);
 
                 List<WordFrequencyInfo> wordFrequencyInfoList = new ArrayList<>();
                 for (String word : words) {
                     WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(word, 1);
                     wordFrequencyInfoList.add(wordFrequencyInfo);
                 }
-                //get the map for the next step of sizing the same word
-//                Map<String, List<WordFrequencyInfo>> map = getListMap(wordFrequencyInfoList);
-
-//                List<WordFrequencyInfo> frequencyInfos = new ArrayList<>();
-//                for (Map.Entry<String, List<WordFrequencyInfo>> entry : map.entrySet()) {
-//                    WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(entry.getKey(), entry.getValue().size());
-//                    frequencyInfos.add(wordFrequencyInfo);
-//                }
                 wordFrequencyInfoList = frequencyInfos(wordFrequencyInfoList)
                         .stream()
                         .sorted((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount())
@@ -40,20 +31,6 @@ public class WordFrequencyGame {
             }
         }
     }
-//    private List<WordFrequencyInfo> sortWordFrequencyList(List<WordFrequencyInfo> wordFrequencyInfoList) {
-//        return wordFrequencyInfoList.stream()
-//                .collect(Collectors.groupingBy(WordFrequencyInfo::getWord))
-//                .entrySet().stream()
-//                .map(entry -> new WordFrequencyInfo(entry.getKey(), entry.getValue().size()))
-//                .sorted(Comparator.comparingInt(WordFrequencyInfo::getWordCount).reversed())
-//                .collect(Collectors.toList());
-//    }
-//List<WordFrequencyInfo> frequencyInfos = new ArrayList<>();
-//                for (Map.Entry<String, List<WordFrequencyInfo>> entry : map.entrySet()) {
-//        WordFrequencyInfo wordFrequencyInfo = new WordFrequencyInfo(entry.getKey(), entry.getValue().size());
-//        frequencyInfos.add(wordFrequencyInfo);
-//    }
-//    wordFrequencyInfoList = frequencyInfos;
 
     private List<WordFrequencyInfo> frequencyInfos(List<WordFrequencyInfo> wordFrequencyInfoList) {
         return wordFrequencyInfoList.stream()
@@ -70,19 +47,11 @@ public class WordFrequencyGame {
                 .collect(Collectors.joining(NEWLINE_DELIMITER));
     }
 
-    private Map<String, List<WordFrequencyInfo>> getListMap(List<WordFrequencyInfo> wordFrequencyInfoList) {
+    private Map<String, List<WordFrequencyInfo>> createWordFrequencyMap(List<WordFrequencyInfo> wordFrequencyInfoList) {
         Map<String, List<WordFrequencyInfo>> map = new HashMap<>();
         for (WordFrequencyInfo wordFrequencyInfo : wordFrequencyInfoList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordFrequencyInfo.getWord())) {
-                List<WordFrequencyInfo> frequencyInfoList = new ArrayList<>();
-                frequencyInfoList.add(wordFrequencyInfo);
-                map.put(wordFrequencyInfo.getWord(), frequencyInfoList);
-            } else {
-                map.get(wordFrequencyInfo.getWord()).add(wordFrequencyInfo);
-            }
+            map.computeIfAbsent(wordFrequencyInfo.getWord(), k -> new ArrayList<>()).add(wordFrequencyInfo);
         }
         return map;
     }
-
 }
